27436ed511553481eb1c2b1f2e4d9d9d9000d845
Frederick Vu <100011202+FrederickVu@users.noreply.github.com>
2025-01-21 18:04:09 -0800

    Update last_drop.py to use BaseCustomProcessor

--- logits_processors/last_drop.py
+++ logits_processors/last_drop.py
@@ -1,23 +1,17 @@
 import torch
-import os
 import json
-from datetime import datetime
-from transformers import LogitsProcessor
+from .base_processor import BaseCustomProcessor
 from scipy.stats import entropy
 
-class LastDropProcessor(LogitsProcessor):
+class LastDropProcessor(BaseCustomProcessor):
     """
     Filter out tokens after the latest drop in logit values above a given threshold. 
     """
-    def __init__(self, *, k=100, threshold=0.2, analysis_mode=None, model_name=None):
+    def __init__(self, *, k=100, threshold=0.2, **kwargs):
         self.k = k
         self.threshold = threshold
 
-        self.analysis_mode = analysis_mode
-        self.model_name = model_name
-
-        if self.analysis_mode:
-            self._setup_analysis_directory()
+        super().__init__(**kwargs)
     
     def __call__(self, past_token_ids, logits):
         logits = logits.squeeze(0)
@@ -43,26 +37,6 @@ class LastDropProcessor(LogitsProcessor):
             )
 
         return logits.unsqueeze(0)
-    
-    def _setup_analysis_directory(self):
-        """Creates a subdirectory for the logs, named based on model and timestamp."""
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        model_name_clean = (self.model_name).split("/")[-1]
-        self.current_subdir = os.path.join("analysis", "last_drop", f"{model_name_clean}_{timestamp}")
-        os.makedirs(self.current_subdir, exist_ok=True)
-
-        metadata_path = os.path.join(self.current_subdir, "metadata.json")
-        metadata = {
-            "model_name": self.model_name,
-            "timestamp": timestamp,
-            "k": self.k,
-            "threshold": self.threshold,
-        }
-        with open(metadata_path, "w", encoding="utf-8") as f:
-            json.dump(metadata, f, indent=4)
-
-        self.log_file_path = os.path.join(self.current_subdir, "last_drop.jsonl")
-        self.log_file = open(self.log_file_path, "a", encoding="utf-8")
 
     def _write_log(
             self,
@@ -91,20 +65,3 @@ class LastDropProcessor(LogitsProcessor):
         }
         self.log_file.write(json.dumps(out_data) + "\n")
         self.log_file.flush()
-
-    def close_log(self):
-        if self.log_file:
-            self.log_file.close()
-            self.log_file = None
-
-    def set_param(self, key, value):
-        if key == "k":
-            self.k = int(value)
-        elif key == "threshold":
-            self.threshold = float(value)
-
-    def get_param_names(self):
-        return {
-            "k": self.k,
-            "threshold": self.threshold
-        }
